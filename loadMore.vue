9800136c7e2c6386517351393f5c2cfcd9d6c5fc
Myyidagit <xudongming1020@163.com>
2020-09-04 16:57:00 +0800

    'xu'

---
+++ loadMore.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-loadmore="loadmore">
+    <div class="outBox">
+      <li v-for="item in num">8888</li>
+      <p>加载中</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      num: 10
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    loadmore() {
+      this.num += 10;
+    }
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="scss" scoped>
+.outBox {
+  height: 100px;
+  width: 100px;
+  overflow: auto;
+}
+</style>
